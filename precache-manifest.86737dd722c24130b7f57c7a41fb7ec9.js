866631f1b7ee95279823bb3fa1983fcb81f8c971
boom40 <kickgames675@gmail.com>
2020-03-05 01:32:01 +0200

    Updates

---
+++ precache-manifest.86737dd722c24130b7f57c7a41fb7ec9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ed14e82f1f1baa2c5be999c0c495dac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8373eb40a19ab6ce218",
+    "url": "/static/css/main.95ab332f.chunk.css"
+  },
+  {
+    "revision": "693e296ef1d5a933a38c",
+    "url": "/static/js/2.71fe41f0.chunk.js"
+  },
+  {
+    "revision": "4214db9a6c23cd334176e62fe82579f5",
+    "url": "/static/js/2.71fe41f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8373eb40a19ab6ce218",
+    "url": "/static/js/main.41c548e1.chunk.js"
+  },
+  {
+    "revision": "342c1f54672c8b280cf9",
+    "url": "/static/js/runtime-main.f159a6bc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cookbook/precache-manifest.78019c5752311fdd06df72040763cd19.js"
+  "/precache-manifest.86737dd722c24130b7f57c7a41fb7ec9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cookbook/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
